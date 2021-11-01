7e1d13fe013290152d4ce69f5bfd28443e12b4dc
Ashish Personal <ashishskkumar321@gmail.com>
2021-11-01 12:59:09 +0530

    firstt commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require("path");
+const splitAndStore = (fileName , endLine) => {
+     let splitNewLine = fs.readFileSync(fileName).toString().split("\n");
+     let totalLines = splitNewLine.length;
+     let data = []
+     return new Promise((resolve , reject) => {
+         if(totalLines <= 0) reject("No data found")
+         if(endLine > totalLines) reject(`total logs found ${totalLines} and desired ${endLine}`)
+         for(let i = totalLines - endLine; i < totalLines - 1; i++) {
+             data.push(splitNewLine[i]);
+         }
+         resolve(data)
+     })
+}
+
+const readDir = (dirName) => {
+    const directoryPath = path.join(__dirname, dirName);
+    return new Promise((resolve , reject) => {
+        fs.readdir(directoryPath , (err , files) => {
+            if(err) {
+                reject(`Unable to read directory ${err}`)
+            } else {
+                resolve(files)
+            }
+        })
+    })
+}
+
+
+
+
+module.exports = {
+   readDir,
+   splitAndStore
+}
+
